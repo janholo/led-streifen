57f0407c1d644a72fe0b6c0db5f0c3073bbc58f8
Jan Reinhardt <jan.reinhardt1@gmx.de>
2023-01-04 23:37:40 +0100

    add loading of thing

--- src/components/SwitchOnOff.tsx
+++ src/components/SwitchOnOff.tsx
@@ -1,5 +1,5 @@
 import { setDefaultResultOrder } from "dns";
-import { HTMLProps, useState } from "react"
+import { HTMLProps, useEffect, useState } from "react"
 import Switch from "react-switch";
 
 type SwitchOnOffProps = {
@@ -7,9 +7,35 @@ type SwitchOnOffProps = {
 
 const SwitchOnOff = (props: SwitchOnOffProps) => {
 
-  let [loading, setLoading] = useState<boolean>(false);
+  let [loading, setLoading] = useState<boolean>(true);
   let [lightOn, setLightOn] = useState<boolean>(false);
   let [error, setError] = useState<boolean>(false);
+
+  function queryState() {
+    if(document.hidden === true) {
+      return;
+    }
+
+    fetch(`http://wood-light/state`, { method: "GET" })
+    .then(
+      (response) => response.json(),
+      () => { setLoading(false); setError(true)})
+      .then((json) => {
+        let isOn = json as number === 1 ? true : false;
+        setLoading(false);
+        console.log(json);
+        setLightOn(isOn);
+      }); 
+  }
+
+  useEffect(() => {
+    queryState();
+    
+    document.addEventListener("visibilitychange", queryState, false)
+    return () => {
+      document.removeEventListener("visibilitychange", queryState)
+    }
+  }, [])
 
   function switchLight(on: boolean) {
     setError(false);
